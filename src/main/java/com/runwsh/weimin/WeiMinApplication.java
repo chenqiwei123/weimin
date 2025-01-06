cd5875207e321b64e83f8d7887db3b52eee6a6e7
RunWsh <runcqw@163.com>
2025-01-06 23:50:13 +0800

    适配jdk8、添加项目标识

---
+++ src/main/java/com/runwsh/weimin/WeiMinApplication.java
@@ -0,0 +1,13 @@
+package com.runwsh.weiMin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiMinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiMinApplication.class, args);
+    }
+
+}
