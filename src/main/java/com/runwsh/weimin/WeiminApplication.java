baf3ddda05b5007bb989081238211ae39c82af1e
RunWsh <runcqw@163.com>
2025-01-06 22:28:55 +0800

    Initial WeiMin Project commit

---
+++ src/main/java/com/runwsh/weimin/WeiminApplication.java
@@ -0,0 +1,13 @@
+package com.runwsh.weimin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runwsh/weimin/WeiminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runwsh.weimin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
